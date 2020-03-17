18d38de647687e1a9fa43925f9e29bd409f9be43
Valentin <valentin-didomenico@orange.fr>
2020-03-17 17:48:26 +0100

    [ADD] New scripts for listeners management

---
+++ Assets/Scripts/Audio/PostWwiseEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostWwiseEvent : MonoBehaviour
+{
+    void Start()
+    {
+        AudioManager.instance.AddListeners(this.gameObject, 0, 1);
+        AkSoundEngine.PostEvent("Play_Main_Music", this.gameObject);
+    }
+}
