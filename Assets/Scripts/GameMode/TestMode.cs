efb3e804e408d5592872b5e632159111fcfe6fc6
Fabpk90 <fabrizio1996@live.fr>
2020-03-18 22:26:36 +0100

    [ADD] WIP camera setups

---
+++ Assets/Scripts/GameMode/TestMode.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInputManager))]
+public class TestMode : MonoBehaviour
+{
+    public static TestMode instance;
+
+    public PlayerInput playerPrefab;
+    
+    public List<PlayerInput> players;
+
+    public bool debugPlayers;
+    
+    private PlayerInputManager _manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            
+            _manager = GetComponent<PlayerInputManager>();
+
+            _manager.playerPrefab = playerPrefab.gameObject;
+            
+            _manager.onPlayerJoined += OnPlayerJoined;
+
+            if (!debugPlayers)
+            {
+                print(Gamepad.all.Count);
+
+                //we add the 4 players, or less depending on connected gamepads
+                for (int i = 0; i < Gamepad.all.Count && i < 4; i++)
+                {
+                    _manager.JoinPlayer(i, i, "GamePads", Gamepad.all[i]);
+                }
+            }
+            
+            for (int i = 0; i < 4; i++)
+            {
+                _manager.JoinPlayer(i, i, "GamePads", Gamepad.all[0]);
+            }
+            
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnPlayerJoined(PlayerInput obj)
+    {
+        Debug.Log("Player " + obj.playerIndex + " joined !");
+        
+        players.Add(obj);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _manager.JoinPlayer(5, 5, "GamePads", Gamepad.all[0]);
+        }
+    }
+}
