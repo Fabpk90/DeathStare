e90414324a0f84a8f993a3e8911a431928a609ee
Valentin <valentin-didomenico@orange.fr>
2020-04-04 16:17:23 +0200

    [ADD] Unity project : sound scenes + basic integration

--- Assets/Scripts/Audio/PostWwiseEvent.cs
+++ Assets/Scripts/Audio/PostWwiseEvent.cs
@@ -6,7 +6,17 @@ public class PostWwiseEvent : MonoBehaviour
 {
     void Start()
     {
-        AudioManager.instance.AddListeners(this.gameObject, 0, 1);
+        AudioManager.instance.AddListeners(this.gameObject, 0);
         AkSoundEngine.PostEvent("Play_Main_Music", this.gameObject);
+    }
+
+    public void Play_Foleys_Character1_Run_Footsteps()
+    {
+        AkSoundEngine.PostEvent("Play_Foleys_Character1_Run_Footsteps", this.gameObject);
+    }
+
+    public void Play_Foleys_Character1_Walk_Footsteps()
+    {
+        AkSoundEngine.PostEvent("Play_Foleys_Character1_Walk_Footsteps", this.gameObject);
     }
 }
