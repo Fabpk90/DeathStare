79d443bf07b4dd5435fef4f006afd078fa3c6281
Nicolas <n.leray94@gmail.com>
2020-04-17 16:35:43 +0200

    [ADD] spawnet + deathtrigger + inté sur Map

---
+++ Assets/Scripts/Actor/Props/DeathTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Actor.Player;
+public class DeathTrigger : MonoBehaviour
+{
+	private void OnTriggerStay(Collider other)
+	{
+		PlayerHealth hp = other.transform.root.GetComponent<PlayerHealth>();
+		if (!hp) return;
+		Debug.Log("kill " + hp.name);
+		//hp.Die(); no effect
+		hp.TakeDamage(other.transform.root.GetComponent<PlayerController>().GetPlayerIndex(), 99999);
+	}
+}
