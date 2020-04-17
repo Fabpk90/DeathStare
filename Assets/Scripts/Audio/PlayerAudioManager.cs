9e86d848c8a887d8bd7a1aaddcd37ccf172402bb
Valentin <valentin-didomenico@orange.fr>
2020-04-17 19:34:14 +0200

    [UPDATE] Integration done : stingers, fb kills, 1st foleys

--- Assets/Scripts/Audio/PlayerAudioManager.cs
+++ Assets/Scripts/Audio/PlayerAudioManager.cs
@@ -8,33 +8,41 @@ public class PlayerAudioManager : MonoBehaviour
     public GameObject leftFoot;
     public GameObject rightFoot;
     private int playerIndex;
-    public GameObject camera;
+    public WwiseListener wwiseListener;
     
     private void Start()
     {
-        AudioManager.instance.AddListeners(leftFoot, 4);
-        AudioManager.instance.AddListeners(rightFoot, 4);
+        AudioManager.instance.AddListeners(leftFoot, 0, 1, 2, 3);
+        AudioManager.instance.AddListeners(rightFoot, 0, 1, 2, 3);
         playerIndex = player.GetPlayerIndex();
         switch (playerIndex)
         {
             case (0):
                 GetComponent<SetWwiseSwitch>().SetSwitch("Cobble", leftFoot);
                 GetComponent<SetWwiseSwitch>().SetSwitch("Cobble", rightFoot);
+                wwiseListener.VolumesOffset[0] = 0;
+                wwiseListener.VolumesOffset[1] = -20;
                 Debug.Log("Cobble set to p1");
                 break;
             case (1):
                 GetComponent<SetWwiseSwitch>().SetSwitch("Dirt", leftFoot);
                 GetComponent<SetWwiseSwitch>().SetSwitch("Dirt", rightFoot);
+                wwiseListener.VolumesOffset[0] = -20;
+                wwiseListener.VolumesOffset[1] = 0;
                 Debug.Log("Dirt set to p2");
                 break;
             case (2):
                 GetComponent<SetWwiseSwitch>().SetSwitch("Grass", leftFoot);
                 GetComponent<SetWwiseSwitch>().SetSwitch("Grass", rightFoot);
+                wwiseListener.VolumesOffset[0] = 0;
+                wwiseListener.VolumesOffset[1] = -20;
                 Debug.Log("Grass set to p3");
                 break;
             case (3):
                 GetComponent<SetWwiseSwitch>().SetSwitch("Wood", leftFoot);
                 GetComponent<SetWwiseSwitch>().SetSwitch("Wood", rightFoot);
+                wwiseListener.VolumesOffset[0] = -20;
+                wwiseListener.VolumesOffset[1] = 0;
                 Debug.Log("Wood set to p4");
                 break;
         }
